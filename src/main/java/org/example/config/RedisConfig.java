fd65d0ca8e233a4cf25c91fe5b04d5bd6aef6923
yao_xi <289580037@qq.com>
2026-05-16 12:31:37 +0800

    redis 修改

--- src/main/java/org/example/config/RedisConfig.java
+++ src/main/java/org/example/config/RedisConfig.java
@@ -1,13 +1,10 @@
 package org.example.config;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -20,18 +17,15 @@ public class RedisConfig {
 
         // 字符串序列化
         StringRedisSerializer stringSerializer = new StringRedisSerializer();
-        // JSON序列化
-        Jackson2JsonRedisSerializer<Object> jacksonSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        jacksonSerializer.setObjectMapper(om);
+        // JSON序列化 - 使用GenericJackson2JsonRedisSerializer替代过时的Jackson2JsonRedisSerializer
+        GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
 
         // key 和 hashkey 使用字符串序列化
         template.setKeySerializer(stringSerializer);
         template.setHashKeySerializer(stringSerializer);
         // value 和 hashvalue 使用JSON序列化
-        template.setValueSerializer(jacksonSerializer);
-        template.setHashValueSerializer(jacksonSerializer);
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
 
         template.afterPropertiesSet();
         return template;
